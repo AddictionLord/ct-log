5f13cf1a5f3022263e2c7124eb95fc72ba0e6ede
Marek Provaznik <marousiinek@gmail.com>
2025-07-02 07:59:36 +0200

    Add dataset preprocessing script

---
+++ scripts/preprocess_dataset.py
@@ -0,0 +1,62 @@
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import torch
+import torchvision
+from tqdm import tqdm
+
+from src.dataset.ct_log_mask_preprocessor import CTLogMaskPreprocessor
+
+if TYPE_CHECKING:
+    from PIL.Image import Image
+
+
+def preprocess_dataset(src_dir: Path, out_dir: Path, workers: int = 2) -> None:
+    """_summary_
+
+    Args:
+        src_dir: _description_
+        out_dir: _description_
+        workers: _description_. Defaults to 2.
+    """
+    to_pil_image = torchvision.transforms.ToPILImage()
+    out_dir.mkdir(parents=True, exist_ok=True)
+    dataloader = torch.utils.data.DataLoader(
+        CTLogMaskPreprocessor(data_dir=src_dir), batch_size=1, shuffle=False, num_workers=workers,
+    )
+
+    for _, batch in enumerate(tqdm(dataloader, desc="Processing dataset", unit="item")):
+        mask, path = batch["mask"][0], Path(batch["path"][0])
+
+        mask_image: Image = to_pil_image(mask.to(torch.float32))
+        mask_image.save(out_dir / path.name)
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--source_data_dir",
+        type=Path,
+        default="data/raw/set_24",
+        help="Directory containing the dataset.",
+    )
+    parser.add_argument(
+        "--output_data_dir",
+        type=Path,
+        default="data/processed/set_24",
+        help="Directory to save the processed dataset.",
+    )
+    parser.add_argument(
+        "--num_workers",
+        type=int,
+        default=2,
+        help="Number of worker threads for data loading.",
+    )
+    args = parser.parse_args()
+
+    preprocess_dataset(args.source_data_dir, args.output_data_dir / "mask")
+
+
+if __name__ == "__main__":
+    main()
