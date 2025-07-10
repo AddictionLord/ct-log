4fa08ebbe3a3491decdd10c5617fd6eff27b9318
Marek Provaznik <marousiinek@gmail.com>
2025-07-10 21:24:24 +0200

    Add resolution parameter to CTLogDataset initialization

--- scripts/visualize_dataset.py
+++ scripts/visualize_dataset.py
@@ -4,13 +4,14 @@ from src.dataset.ct_log_dataset import CTLogDataset
 
 
 def main() -> None:
-    dataset = CTLogDataset(data_dir="data/processed/set_24", num_classes=10)
+    dataset = CTLogDataset(data_dir="data/processed/set_24", num_classes=10, resolution=(458, 530))
 
     print(f"Dataset length: {len(dataset)}")
 
     for i in range(len(dataset)):
         item = dataset[i]
 
+        px.imshow(item["image"].permute(1, 2, 0), title=str(item["path"])).show()
         px.imshow(item["mask"], title=str(item["path"])).show()
 
         break
